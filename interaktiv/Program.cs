01b6f1c7727b8d4a63352e7e55ac928a9b780292
TE23B-Gissle-Jack <jack.gissle@elev.ga.ntig.se>
2024-09-03 13:29:06 +0200

    snyggt

--- interaktiv/Program.cs
+++ interaktiv/Program.cs
@@ -2,9 +2,9 @@
 using System.Runtime.Versioning;
 
 
-int fan = 0;
-string faklan = "";
-bool fakla = false;
+int hogerAmt = 0;
+string facklan = "";
+bool fackla = false;
 Console.ForegroundColor = ConsoleColor.Cyan;
 Console.WriteLine($"Du står i mitten av en labyrint. Det finns 3 vägar du kan ta, Höger, Vänster och framåt. Vilken Väg tar du?");
 
@@ -17,21 +17,29 @@ firstChoice();
     val = val.ToLower();
     Console.ForegroundColor = ConsoleColor.Cyan;
     if(val == "höger"){
-    Console.WriteLine($"Du går höger. du kommer till ytligare en korsning, på en av väggarna sitter en fakla. Du kan ta faklan,gå höger eller   gå vänster");
+    Console.WriteLine("Du går höger. du kommer till ytligare en korsning, på en av väggarna sitter en fackla. Du kan ta facklan,gå höger eller\ngå vänster");
     hoger();
 }
 else if(val == "vänster"){
-    Console.WriteLine($"Du går vänster. du kommer fram till ett sklett, famför dig fortsätter gången. Du kan Inspectera sklettet eller fortsätta gå");
+    Console.WriteLine($"Du går vänster. du kommer fram till ett skelett, framför dig fortsätter gången. Du kan Inspektera skelettet eller fortsätta gå");
     venster();
 }
 else if(val == "framåt"){
     Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"du går framåt men efter bara ett par steg så skjuter spjut ut ur väggen och dödar dig");
+    Console.Clear();
+    Console.WriteLine($"\n\n\n\n\n\n\n\n\ndu går framåt men efter bara ett par steg så skjuter spjut ut ur väggen och dödar dig");
+    Console.ReadLine();
+}
+else if(val== "bakåt"|| val == "bak")
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("\n\n\n\n\n\n\n\n\n\n                                                           FUCK YOU!");
     Console.ReadLine();
 }
 else{
     Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine($"Skriv antingen, 'Farmåt', 'Höger' eller 'Vänster' för att välja en väg");
+    Console.WriteLine($"Skriv antingen, 'Framåt', 'Höger' eller 'Vänster' för att välja en väg");
     firstChoice();
     }
 }
@@ -41,18 +49,18 @@ void venster(){
     string val = Console.ReadLine();
     val = val.ToLower();
     Console.ForegroundColor = ConsoleColor.Cyan;
-    if(val == "inspectera skelletet"||val=="inspectera"||val=="sklett"){
+    if(val == "inspectera skelettet"||val=="inspectera"||val=="skelett"){
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Du väljer att inspectera sklettet. Du sätter dig på huk och söker igenom sklettets kländer, medans du gör det rör sklettet plötsligt på sig och biter ner i din nacke. Du blöder till döts.");
+        Console.WriteLine($"Du väljer att inspektera skelettet. Du sätter dig på huk och söker igenom skelettets kländer, medan du gör det rör skelettet plötsligt på sig och biter ner i din nacke. Du blöder till döts.");
         Console.ReadLine();
     }
     else if(val == "fortsätt"|| val =="framåt"){
-        Console.WriteLine($"Du fortsätter längs gången. Du kommer tillsist till ett rum. Rummet har ett hål i sig med ett rep, hålet går igenom taket och golvet. Klätrar du up för repet eller ner?");
+        Console.WriteLine($"Du fortsätter längs gången. Du kommer till sist till ett rum. Rummet har ett hål i sig med ett rep, hålet går igenom taket och golvet. Klättrar du upp för repet eller ner?");
         fram();
     }
     else{
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"Skriv antingen, 'Farmåt' eller 'inspectera' för att välja");
+        Console.WriteLine($"Skriv antingen, 'Framåt' eller 'inspectera' för att välja");
         venster();
     }
 
@@ -62,16 +70,18 @@ void venster(){
         val = val.ToLower();
         Console.ForegroundColor = ConsoleColor.Red;
         if(val =="up"){
-            Console.WriteLine($"Du klättar up för repet. Du börjar klätra up för repet men just som det ser ut som du nåt slutet av det så losnar det och du faller till din död.");
+            Console.Clear();
+            Console.WriteLine($"\n\n\n\n\n\n\n\n\nDu klättrar upp för repet. Du börjar klättra upp för repet men just som det ser ut som du nått slutet av det så lossnar det och du faller till din död.");
             Console.ReadLine();
         }
         else if(val == "ner"){
-            Console.WriteLine($"Du klättar ner för repet. Efter att ha klätrat i timmar och utan strykan att klätra längre släpper du taget. Du faller till din död");
+            Console.Clear();
+            Console.WriteLine($"\n\n\n\n\n\n\n\n\nDu klättrar ner för repet. Efter att ha klättrat i timmar och utan styrkan att klättra längre släpper du taget. Du faller till din död");
             Console.ReadLine();
         }
         else{
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"Skriv antingen, 'ner' eller 'up' för att välja");
+            Console.WriteLine($"Skriv antingen, 'ner' eller 'upp' för att välja");
             fram();
         }
     }
@@ -82,20 +92,21 @@ void venster(){
      string val = Console.ReadLine(); 
      val = val.ToLower();
      Console.ForegroundColor = ConsoleColor.Cyan;
-     if(val == "ta faklan"){
+     if(val == "ta facklan"){
         Fakla();
         hoger();
      }
      else if(val == "höger"){
         string tecx = "Du tar den högra vägen. Du kommer till en mörk kosrning. Du kan se en väg åt vänster och en framåt.";
-        Console.WriteLine(tecx + faklan);
+        Console.WriteLine(tecx + facklan);
         hoger2();
      }
      else if(val == "vänster"){
          string imse = "Du går gången till vänster och kommer till ett stort rum fullt av spindelnät";
-        if(fakla == true){
+        if(fackla == true){
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"{imse} Du håller din fakla högt för att se allt   spindelnät och råkar sätta eld på det. Du hör ett otäkt lätte högt uppe i nätet och en gigantisk spindel faller ner och mossar dig. Du är död");
+            Console.WriteLine($"\n\n\n\n\n\n\n\n\n{imse} Du håller din fackla högt för att se allt\nspindelnät och råkar sätta eld på det. Du hör ett otäckt läte högt uppe i nätet och en gigantisk spindel faller ner och mossar dig. Du är död");
         }else{
             Console.WriteLine($"{imse} Du navigerar dig genom rummet utan att röra nätet. Du kommer till två vägar, en som går nedåt och en uppåt. Vilken väljer du?");
         }
@@ -103,13 +114,13 @@ void venster(){
      }
      else{
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"Skriv antingen, 'ta faklan', 'Höger' eller 'Vänster' för att välja");
+        Console.WriteLine($"Skriv antingen, 'ta facklan', 'Höger' eller 'Vänster' för att välja");
         hoger();
     }
       void Fakla(){
-        faklan = " Med faklan kan du   också se en vägg åt höger";
-        fakla = true;
-        Console.WriteLine($"Du tar Faklan. Går du nu Höger eller Vänster?");
+        facklan = " Med facklan kan du\nockså se en vägg åt höger";
+        fackla = true;
+        Console.WriteLine($"Du tar Facklan. Går du nu Höger eller Vänster?");
         Console.ForegroundColor = ConsoleColor.DarkYellow;
      }
 
@@ -124,7 +135,8 @@ void venster(){
         }
         else if(val == "vänster"){
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Du tar en vänstra vägen. direkt efter du går in i gången så rasar den bakom dig. Efter att du har gåt en stund uptäker du att det är en återvänds gränd. Du dör pga brist av vatten.");
+            Console.Clear();
+            Console.WriteLine($"\n\n\n\n\n\n\n\n\nDu tar en vänstra vägen. Direkt efter du går in i gången så rasar den bakom dig. Efter att du har gått en stund upptäcker du att det är en återvändsgränd. Du dör pga brist av vatten.");
             Console.ReadLine();
         }
         else if(val == "höger"){
@@ -145,15 +157,15 @@ void venster(){
                 venster();
             }
             else if(val == "höger"){
-                if(fan < 10){
-                    fan++;
+                if(hogerAmt < 5){
+                    hogerAmt++;
                     Console.WriteLine($"Du tar den högra vägen. Du kommer till en korsning med vägar åt höger eller vänster");
                     hoger3();
                     Console.ReadLine();
                 }
                 else{
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Right is always Right. Du har vunit! Labyrinten gav upp!");
+                    Console.WriteLine($"Right is always Right. Du har vunnit! Labyrinten gav upp!");
                 }
             }
             else{
@@ -171,12 +183,13 @@ void venster(){
         Console.ForegroundColor = ConsoleColor.Cyan;
 
         if(val == "ner"|| val == "nedåt"){
+            Console.Clear();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Du väljer vägen nedåt. Vägen blir brantare och brantare. Du snubblar och lyckas inte stanna tills du kommer till slutet av gången och slår i huvudet och bryter nacken. Du är Död.");
+            Console.WriteLine($"\n\n\n\n\n\n\n\n\nDu väljer vägen nedåt. Vägen blir brantare och brantare. Du snubblar och lyckas inte stanna tills du kommer till slutet av gången och slår i huvudet och bryter nacken. Du är Död.");
         }
         else if(val == "up"|| val =="uppåt"){
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Du tar vägen uppåt. Du går i vad som känns som en oändlighet men tillsist kommer du till ytan. Du har lyckats fly       labyrinten!");
+            Console.WriteLine($"Du tar vägen uppåt. Du går i vad som känns som en oändlighet men tillsist kommer du till ytan. Du har lyckats fly\nlabyrinten!");
         }else{
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine($"Skriv antingen, 'up' eller 'ner' för att välja");
